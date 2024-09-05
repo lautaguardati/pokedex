59e93556cdb025321b718668c25f08da93811d7e
lautaguardati <lautaguardati@gmail.com>
2024-09-04 22:51:39 -0300

    Eliminación líneas innecesarias.
    Al cambiar el paginador dejamos de usar dos llamados al api, reemplazandolo por uno solo.

--- src/services/api.js
+++ src/services/api.js
@@ -6,14 +6,6 @@ export function obtenerImagen(imagen) {
   return (fetch(imagen).then((respuesta) => respuesta.blob()));
 }
 
-export async function obtenerPaginaSiguiente(urlPaginaSiguiente) {
-  return ((await fetch(urlPaginaSiguiente)).json());
-}
-
-export async function obtenerPaginaAnterior(urlPaginaAnterior) {
-  return ((await fetch(urlPaginaAnterior)).json());
-}
-
 export function obtenerListaDePokemons(offset = 0, limite = 20) {
   return (fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limite}`)
     .then((resultado) => resultado.json()));
